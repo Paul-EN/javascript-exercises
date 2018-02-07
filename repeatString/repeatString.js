eb4619fbfe68ed42a2a8a3a3eebd1cbee48efcd0
Paul <g54b@live.com>
2018-02-07 00:36:16 -0700

    Pass all tests for helloWorld, repeatString, and reverseString exercises

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,16 @@
-var repeatString = function() {
+var repeatString = function(str, num) {
+  let newString = '';
 
+  if (num > 0) {
+    for (let i = 0; i < num; i++) {
+      newString = newString.concat(str);
+    }
+    return newString;
+  } else if (num === 0) {
+    return '';
+  } else {
+    return 'ERROR';
+  }
 }
 
 module.exports = repeatString
