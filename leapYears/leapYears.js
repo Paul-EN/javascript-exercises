d796dd93e363a3c5c90177118ba26f47c99607f8
Paul <g54b@live.com>
2018-02-08 14:04:22 -0700

    Complete leapYear javascript exercise and pass all tests

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,35 @@
-var leapYears = function() {
+var leapYears = function(year) {
+  function checkIfCentury() {
+    return year % 100 === 0;
+  }
 
+  function checkCenturyLeap() {
+    if (year < 400) {
+      return false;
+    } else if (year >= 400 && year % 400 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function checkNonCenturyLeap() {
+    if (year % 4 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function mainCheck() {
+    if (checkIfCentury()) {
+      return checkCenturyLeap();
+    } else {
+      return checkNonCenturyLeap();
+    }
+  }
+
+  return mainCheck();
 }
 
 module.exports = leapYears
