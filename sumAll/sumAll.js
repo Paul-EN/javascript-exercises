a12eae36d2db8eb0e7866a445c5e4d2050a31b61
Paul <g54b@live.com>
2018-02-07 22:11:27 -0700

    Complete sumAll exercise and pass all tests

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,40 @@
-var sumAll = function() {
+var sumAll = function(x, y) {
+  function verifyParameters() {
+    if (typeof x === 'number' && typeof y === 'number' && x >= 0 && y >= 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
+  function leastGreatest() {
+    let sortedArray = [];
+
+    if (x < y) {
+      sortedArray.push(x);
+      sortedArray.push(y);
+    } else if (x > y) {
+      sortedArray.push(y);
+      sortedArray.push(x);
+    }
+
+    return sortedArray;
+  }
+
+  function sum() {
+    let result = 0;
+    if (verifyParameters() === true) {
+      for (let i = leastGreatest()[0]; i <= leastGreatest()[1]; i++) {
+        result += i;
+      }
+    } else {
+      return 'ERROR';
+    }
+
+    return result;
+  }
+
+  return sum();
 }
 
 module.exports = sumAll
